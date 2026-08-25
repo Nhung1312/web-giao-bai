b16b9b899500730ceca7967d62b55fc335d62cb8
Nhung1312 <nhung.ngo.u@gmail.com>
2026-08-25 10:13:56 +0700

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -7,15 +7,30 @@ import {
   onAuthStateChanged,
   User 
 } from 'firebase/auth';
-import { getFirestore, collection, doc, getDoc, getDocs, setDoc, addDoc, updateDoc, deleteDoc, query, where, orderBy, onSnapshot, serverTimestamp } from 'firebase/firestore';
+import { 
+  getFirestore, 
+  collection, 
+  doc, 
+  getDoc, 
+  getDocs, 
+  setDoc, 
+  addDoc, 
+  updateDoc, 
+  deleteDoc, 
+  query, 
+  where, 
+  orderBy, 
+  onSnapshot, 
+  serverTimestamp 
+} from 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCJaQgSfMYC1RqLeCXWT_SJrQWlLs7PSkg",
-  authDomain: "onluyenpro-53b11.firebaseapp.com",
-  projectId: "onluyenpro-53b11",
-  storageBucket: "onluyenpro-53b11.firebasestorage.app",
-  messagingSenderId: "271702658864",
-  appId: "1:271702658864:web:3356c5212459347dc9c2f5"
+export const firebaseConfig = {
+  apiKey: "AIzaSyBp8x8uUnx2wqEC3m0kb8_0r8Hr62AOX04",
+  authDomain: "toan-thcs-2026.firebaseapp.com",
+  projectId: "toan-thcs-2026",
+  storageBucket: "toan-thcs-2026.firebasestorage.app",
+  messagingSenderId: "103179763579",
+  appId: "1:103179763579:web:a245a638ea9bd2609147de"
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
@@ -24,5 +39,22 @@ export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
 googleProvider.setCustomParameters({ prompt: 'select_account' });
 
-export { signInWithPopup, signOut, onAuthStateChanged, collection, doc, getDoc, getDocs, setDoc, addDoc, updateDoc, deleteDoc, query, where, orderBy, onSnapshot, serverTimestamp };
+export { 
+  signInWithPopup, 
+  signOut, 
+  onAuthStateChanged,
+  collection, 
+  doc, 
+  getDoc, 
+  getDocs, 
+  setDoc, 
+  addDoc, 
+  updateDoc, 
+  deleteDoc, 
+  query, 
+  where, 
+  orderBy, 
+  onSnapshot, 
+  serverTimestamp 
+};
 export type { User };
